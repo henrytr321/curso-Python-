ab409afa8cbc69d5448416e9c8fac01529440da9
henrytr321 <henrytr321@gmail.com>
2022-04-03 18:48:26 -0500

    Ejercicio con el ciclo For

---
+++ bucles/for.py
@@ -0,0 +1,18 @@
+# def run():
+#     LIMITE = 1000
+
+#     contador = 0
+#     while contador <= 1000:
+#         print("El numero es: " + str(contador))
+#         contador = contador + 1
+
+# if __name__== "__main__":
+#     run()
+
+
+# for contador in range(1, 1001):
+#     print(contador)
+
+for  i in range(10):
+    print(11 * i)
+    
