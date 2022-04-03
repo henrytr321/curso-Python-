eae0216d6cca15b5da8f4ba7901051b6f669a765
henrytr321 <henrytr321@gmail.com>
2022-04-03 14:24:03 -0500

    funcioando guardando el resultado

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -6,20 +6,29 @@
 # imprimir_mensaje()
 # imprimir_mensaje()
 
-def conversacion(mensaje):
-    print("Hola")
-    print("Cómo estas")
-    print(mensaje)
-    print("Adios")
+# def conversacion(mensaje):
+#     print("Hola")
+#     print("Cómo estas")
+#     print(mensaje)
+#     print("Adios")
     
 
 
-opcion = int(input("Elige una opción (1, 2, 3): "))
-if opcion == 1:
-    conversacion("Egiste la opcion 1")
-elif opcion == 2:
-    conversacion("Egiste la opcion 2")
-elif opcion == 3:
-    conversacion("Egiste la opcion 3")
-else:
-    print("Escribe una opción correcta")
+# opcion = int(input("Elige una opción (1, 2, 3): "))
+# if opcion == 1:
+#     conversacion("Egiste la opcion 1")
+# elif opcion == 2:
+#     conversacion("Egiste la opcion 2")
+# elif opcion == 3:
+#     conversacion("Egiste la opcion 3")
+# else:
+#     print("Escribe una opción correcta")
+
+
+def suma(a, b):
+    print("Se suman dos números")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
