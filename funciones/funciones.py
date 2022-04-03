c8b2d83bf6ecd9180b32001c7186c52aa96f112d
henrytr321 <henrytr321@gmail.com>
2022-04-03 14:16:20 -0500

    Antes de aplicar funciones

---
+++ funciones/funciones.py
@@ -0,0 +1,27 @@
+# def imprimir_mensaje():
+#     print("Mensaje especial: ")
+#     print("¡Estoy aprendiendo a usar funciones!")
+
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+
+opcion = int(input("Elige una opción (1, 2, 3): "))
+if opcion == 1:
+    print("Hola")
+    print("Cómo estas")
+    print("Elegiste la opción 1")
+    print("Adios")
+elif opcion == 2:
+    print("Hola")
+    print("Cómo estas")
+    print("Elegiste la opción 2")
+    print("Adios")
+elif opcion == 3:
+    print("Hola")
+    print("Cómo estas")
+    print("Elegiste la opción 3")
+    print("Adios")
+else:
+    print("Escribe una opción correcta")
